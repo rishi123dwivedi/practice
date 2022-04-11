24819be34cb6f3b8c6c20b68f16ac14aa0a7ea18
rishi123dwived <rdwivedi@deqode.com>
2022-04-11 10:54:12 +0530

    Deleting

--- demo/src/main/java/com/example/demo/dao/StudentDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.demo.model.Student;
-
-public interface StudentDao extends MongoRepository<Student, Integer> {
-
-}
