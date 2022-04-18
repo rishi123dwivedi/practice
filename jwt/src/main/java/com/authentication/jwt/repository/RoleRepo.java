464c5f73ab6295cd85893cf4c0d9640bca349ce1
rishi123dwived <rdwivedi@deqode.com>
2022-04-19 00:07:35 +0530

    added everything in one project

--- jwt/src/main/java/com/authentication/jwt/repository/RoleRepo.java
+++
@@ -1,9 +0,0 @@
-package com.authentication.jwt.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.jwt.authentication.model.Role;
-
-public interface RoleRepo extends MongoRepository<Role, Integer>{
-
-}
--- jwt/src/main/java/com/jwt/authentication/model/ERole.java
+++
@@ -1,6 +0,0 @@
-package com.jwt.authentication.model;
-
-public enum ERole {
-	ROLE_USER, ROLE_MODERATOR, ROLE_ADMIN
-
-}
